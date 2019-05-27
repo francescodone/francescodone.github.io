87dd0b7280811cc91a4666856e1a5dca8f61c6dd
Francesco Donè <francesco.done.11597@gmail.com>
2019-05-27 21:55:06 +0200

    Revert "Updates"
    This reverts commit 786fa392315437bae605d3b99934a1c398d43baa.

--- precache-manifest.ef3bc70e582c50d56eb7e3745e628ba6.js
+++ precache-manifest.ef3bc70e582c50d56eb7e3745e628ba6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9db28ce15e038569d8e19e17cb5e4f9d",
+    "revision": "ef797a34cb42400d602b132d2674da7b",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.8a929360.chunk.css"
   },
   {
-    "revision": "3219c994d9e05801dbba",
-    "url": "/static/css/main.1ce3114e.chunk.css"
+    "revision": "3e2641b514f087d7248a",
+    "url": "/static/css/main.32884d3b.chunk.css"
   },
   {
     "revision": "4f489a410c24c1bb12e0",
     "url": "/static/js/2.6e84d1c9.chunk.js"
   },
   {
-    "revision": "3219c994d9e05801dbba",
-    "url": "/static/js/main.1aaf67e9.chunk.js"
+    "revision": "3e2641b514f087d7248a",
+    "url": "/static/js/main.bc313f37.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
