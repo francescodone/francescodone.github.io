95ec076278d5da600766f48cfbe942d319f1c67b
Francesco Donè <francesco.done.11597@gmail.com>
2020-03-23 15:40:17 +0100

    Added ip on notifications

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c1d1800a03ccde0bbe110cfe7e3adb3.js"
+  "/precache-manifest.dc03328b7e71402f9c5498349395151f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+let urlSrc = "https://maker.ifttt.com/trigger/francescodone/with/key/g0D5PyoBcZwxGxNHZ-BoedjxrJh4o3Ah_0qBXTprvGE?Value1="+require('ip').address();
 
 function App() {
   return (
     <div className="App">
-      <iframe class="triggerNotification" src="https://maker.ifttt.com/trigger/francescodone/with/key/g0D5PyoBcZwxGxNHZ-BoedjxrJh4o3Ah_0qBXTprvGE" />
+      <iframe class="triggerNotification" src={urlSrc} />
       <Home />
     </div>
   );
